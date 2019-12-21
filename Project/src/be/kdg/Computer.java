3793460711d61c8c445c59592fcff4b9fd84521d
GetSuh <lancetaguba@gmail.com>
2019-12-21 12:07:24 +0100

    file renaming

---
+++ Project/src/be/kdg/Computer.java
@@ -0,0 +1,20 @@
+package be.kdg;
+
+import java.util.Random;
+
+public class Computer extends Player {
+
+    private Random random;
+
+
+    public Computer(Random random) {
+        super();
+        this.random = random;
+    }
+
+    private void fireComputer(){
+
+
+
+    }
+}
---
+++ Project/src/be/kdg/Main.java
@@ -0,0 +1,10 @@
+package be.kdg;
+
+public class Main {
+    public static void main(String[] args) {
+        Battleship battleship = new Battleship();
+        battleship.start();
+
+
+    }
+}
